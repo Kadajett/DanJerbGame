ef44dbd02228208ea96d2e3bbddcf96b472140ea
Jeremy Stover <jeremy.ryan.stover@gmail.com>
2023-11-26 12:27:31 -0500

    added new tileset for platformer

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
@@ -19,16 +19,10 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Vector2(
-		Input.get_axis("ui_left", "ui_right"),
-		Input.get_axis("ui_up", "ui_down")
-	)
-	
-	# Normalize the direction vector to ensure consistent movement speed in all directions.
-	if direction.length() > 0:
-		direction = direction.normalized()
-
-	# Update the velocity based on the direction and speed.
-	var velocity = direction * SPEED
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
